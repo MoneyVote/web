17e10eb538f96908127d637249b41845821c8c9d
Drew Eyre <eyreda@colostate.edu>
2021-04-25 02:41:49 -0600

    created the cards component.

---
+++ app/src/Components/Cards/BettingCard.js
@@ -0,0 +1,6 @@
+import {Component} from "react";
+
+export default class BettingCard extends Component {
+
+
+}
---
+++ app/src/Components/Cards/Container.js
@@ -0,0 +1,7 @@
+import {Component} from "react";
+import BettingCard from "./BettingCard";
+
+export default class Container extends Component {
+
+
+}
